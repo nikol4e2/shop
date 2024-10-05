b3ff881279023632a1239fafe41a5cf069997489
nikol4e2 <arsovnikola1@gmail.com>
2024-10-05 13:15:47 +0200

    intisalised project and added models

---
+++ shop/src/main/java/org/example/shop/model/Image.java
@@ -0,0 +1,34 @@
+package org.example.shop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ManyToAny;
+import org.springframework.core.SpringVersion;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    private Product product;
+
+}
